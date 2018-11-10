f3bcd898a0619e303be78c0c38e46846a6d67802
Luis Etchebarne <etchebarneluis@gmail.com>
2018-11-09 23:04:00 -0300

    lui

--- controllers/pregunta.controller.js
+++ controllers/pregunta.controller.js
@@ -502,6 +502,7 @@ exports.generarPreguntasDuelo = function(req, res){
 
 		}else{
 			index.mensaje(user._id.toString(), 'Mensaje', 'Excediste el límite de duelos por día,vuelve a intentarlo mañana!', );
+			return es.json({Mensaje: 'Excediste el límite'});
 		}
 	});
 
